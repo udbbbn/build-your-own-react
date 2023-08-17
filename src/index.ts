b3e1603c8ee6717f85e29b7632712c71eb4b921c
udbbbn <udbbbn@gmail.com>
2023-08-17 11:50:31 +0800

    feat: adding render method

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,25 @@
-type ElementType = keyof HTMLElementTagNameMap
-type ElementProps = Record<string, string | number | null | undefined> | null | undefined
-type ElementChildren = Record<string, string | number | null | undefined> | string
+type EnumElementType = "NODE_ELEMENT"
+type ElementType = keyof HTMLElementTagNameMap | EnumElementType
 const TextElementType = "NODE_ELEMENT"
+interface ElementProps {
+  nodeValue?: string
+  children: DidactElement[]
+}
+type DidactElement = {
+  type: ElementType
+  props: ElementProps
+}
 
 const Didact = {
   createElement,
+  render,
 }
 
-function createElement(type: ElementType, props: ElementProps, ...children: ElementChildren[]) {
+function createElement(
+  type: ElementType,
+  props: ElementProps,
+  ...children: DidactElement[]
+): DidactElement {
   return {
     type,
     props: {
@@ -22,10 +34,10 @@ function createElement(type: ElementType, props: ElementProps, ...children: Elem
 /**
  * In order to simplify code, we specialise in a function to handle primitive data.
  * React doesn't create primitive value or create empty array when children is null.
- * @param text
+ * @param text string
  * @returns
  */
-function createTextElement(text: string) {
+function createTextElement(text: string): DidactElement {
   return {
     type: TextElementType,
     props: {
@@ -33,6 +45,39 @@ function createTextElement(text: string) {
       children: [],
     },
   }
+}
+
+/**
+ * We start by creating the element by element type and then append child to the container.
+ * @param element DidactElement
+ * @param container HTMLElement
+ */
+function render(element: DidactElement, container: HTMLElement) {
+  const dom =
+    element.type === TextElementType
+      ? document.createTextNode("")
+      : document.createElement(element.type)
+
+  /**
+   * We also need to assign property to Dom node.
+   * @param key keyof ElementProps
+   * @returns boolean
+   */
+  const isProperty = (key: string) => key !== "children"
+  Object.keys(element.props)
+    .filter(isProperty)
+    .forEach(key => {
+      Reflect.set(dom, key, element.props[key as keyof ElementProps])
+    })
+  /**
+   * And we recursively do the same for each child.
+   * The children always an empty array when type is TextElementType.
+   */
+  element.props.children.forEach(child => {
+    render(child, dom as HTMLElement)
+  })
+
+  container.appendChild(dom)
 }
 
 export default Didact
--- src/test.tsx
+++ src/test.tsx
@@ -1,6 +1,12 @@
+import Didact from "./index"
+
 const element: any = (
   <div id="foo">
     <a>bar</a>
     <b />
   </div>
 )
+
+Didact.render(element, document.querySelector("#app")!)
+
+export default {}
